4f58cf0ca668763f76bb35d16c255b04ef467eaf
Ahmed-Omar-Hommir <ahmedomarhommir@gmail.com>
2026-03-22 20:10:02 +0200

    feat: add paginated notes provider import to notes list page

--- example/lib/ui/notes_list_page.dart
+++ example/lib/ui/notes_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:example/providers/paginated_notes_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_craft/riverpod_craft.dart';
